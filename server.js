1005931cb1026ceebdee520a22075c3251b44810
mouadkrt <m.sentissi@gmail.com>
2022-07-29 11:45:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function(req, res) {
     console.log("Got request on / endpoint. Sending back a Hello World ....");
-    res.send('Hello from NodeJS  at '+ new Date());
+    res.send('Hello from NodeJS-v2  at '+ new Date());
 });
 
 
