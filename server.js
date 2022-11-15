f006dcd15a8afdb180e484f03d4463dae69d137b
mouadkrt <m.sentissi@gmail.com>
2022-11-15 15:57:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/', function(req, res) {
     res.send('<h3>Hello from NodeJS-v1  at </h3>'+ new Date());
 });
 
+app.get('/end_point1', function(req, res) {
+    console.log("Got request on /end_point1 endpoint. Sending some json content ....");
+    res.json({ company: 'Munisys' });
+});
+
 
 app.listen(8080, ip);
 console.log('Server listening on 8080 ...');
