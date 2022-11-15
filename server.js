6be531d6811d81a94eff7e526c98644866dc2dde
mouadkrt <m.sentissi@gmail.com>
2022-11-15 16:43:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function(req, res) {
     console.log("Got request on / endpoint. Sending back a Hello World ....");
-    res.send('<h3>Hello from NodeJS-v1  at </h3>'+ new Date());
+    res.send('<h3>Hello from NodeJS-v3  at </h3>'+ new Date());
 });
 
-app.get('/end_point1', function(req, res) {
+app.get('/api/end_point1', function(req, res) {
     console.log("Got request on /end_point1 endpoint. Sending some json content ....");
     res.json({ company: 'Munisys' });
+});
+
+app.get('/api/end_point2', function(req, res) {
+    console.log("Got request on /end_point2 endpoint. Sending some json content ....");
+    res.json([{ user: 'Ayoub' }, { user: 'Mouad' }]);
 });
 
 
